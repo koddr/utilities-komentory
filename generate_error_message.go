837e03f011ab96fcee4bd207ef89a31296b3d2ac
Vic Shóstak <vic@shostak.dev>
2021-09-09 08:43:12 +0300

    Add CheckForError func (for Fiber route controllers)

--- generate_error_message.go
+++ generate_error_message.go
@@ -1,29 +1,45 @@
 package utilities
 
-import "fmt"
+import (
+	"fmt"
 
-// GenerateErrorMessage func for generating error messages:
+	"github.com/gofiber/fiber/v2"
+)
+
+// GenerateErrorMessage func for generating error messages.
 //
-// 400 -> incorrect data for the <object> (<context>)
-// 401 -> unauthorized request to the <object> (<context>)
-// 403 -> access denied to the <object> (<context>)
-// 404 -> no <object> found (<context>)
-// 500 -> something went wrong with the <object> (<context>)
-func GenerateErrorMessage(statusCode int, object, context string) string {
-	var errorMessage string
+// 400 -> incorrect data for the <object> (<explanation>)
+// 401 -> unauthorized request to the <object> (<explanation>)
+// 403 -> access denied to the <object> (<explanation>)
+// 404 -> <object> not found (<explanation>)
+// 500 -> something went wrong with the <object> (<explanation>)
+// XXX -> oops... something went wrong, but the status code is not handled (<code>)
+func GenerateErrorMessage(statusCode int, object, explanation string) string {
+	var errMsg string
 	switch statusCode {
-	case 400:
-		errorMessage = fmt.Sprintf("incorrect data for the %s (%s)", object, context)
-	case 401:
-		errorMessage = fmt.Sprintf("unauthorized request to the %s (%s)", object, context)
-	case 403:
-		errorMessage = fmt.Sprintf("access denied to the %s (%s)", object, context)
-	case 404:
-		errorMessage = fmt.Sprintf("no %s found (%s)", object, context)
-	case 500:
-		errorMessage = fmt.Sprintf("something went wrong with the %s (%s)", object, context)
+	case fiber.StatusBadRequest:
+		errMsg = fmt.Sprintf("incorrect data for the %s (%s)", object, explanation)
+	case fiber.StatusUnauthorized:
+		errMsg = fmt.Sprintf("unauthorized request to the %s (%s)", object, explanation)
+	case fiber.StatusForbidden:
+		errMsg = fmt.Sprintf("access denied to the %s (%s)", object, explanation)
+	case fiber.StatusNotFound:
+		errMsg = fmt.Sprintf("%s not found (%s)", object, explanation)
+	case fiber.StatusInternalServerError:
+		errMsg = fmt.Sprintf("something went wrong with the %s (%s)", object, explanation)
 	default:
-		errorMessage = fmt.Sprintf("ooops... unhandled status code (%d)", statusCode)
+		errMsg = fmt.Sprintf("oops... something went wrong, but the status code is not handled (%d)", statusCode)
 	}
-	return errorMessage
+	return errMsg
+}
+
+// CheckForError func for checking error in given function and returning error message by status code.
+func CheckForError(ctx *fiber.Ctx, errFunc error, statusCode int, object, explanation string) error {
+	if errFunc != nil {
+		return ctx.Status(statusCode).JSON(&fiber.Map{
+			"error": true,
+			"msg":   GenerateErrorMessage(statusCode, object, explanation),
+		})
+	}
+	return nil
 }
